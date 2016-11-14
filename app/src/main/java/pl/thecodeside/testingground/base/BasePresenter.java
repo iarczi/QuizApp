fae3e27e76b8adb6660b68d896b5b6a11a9857ae
iarczi <artur.latoszewski@safekiddo.com>
2016-11-14 21:44:48 +0100

    Base View and Presenter

---
+++ app/src/main/java/pl/thecodeside/testingground/base/BasePresenter.java
@@ -0,0 +1,14 @@
+package pl.thecodeside.testingground.base;
+
+/**
+ * Created by Artur Latoszewski on 14.11.2016.
+ */
+
+public interface BasePresenter {
+
+    void subscribe();
+
+    void unsubscribe();
+
+}
+
---
+++ app/src/main/java/pl/thecodeside/testingground/base/BaseView.java
@@ -0,0 +1,9 @@
+package pl.thecodeside.testingground.base;
+
+/**
+ * Created by Artur Latoszewski on 14.11.2016.
+ */
+
+public interface BaseView<T> {
+    void setPresenter(T presenter);
+}
